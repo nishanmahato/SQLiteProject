2eed7b5cfebb774e22fbc6a70e3bdae7692983f4
Nishan Mahato <nsnmah21@gmail.com>
2025-02-10 21:26:23 +0545

    complete databasehelper

--- app/src/main/java/com/example/sqliteproject/DatabaseHelper.java
+++ app/src/main/java/com/example/sqliteproject/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.example.sqliteproject;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -45,5 +46,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }else{
             return true;
         }
+    }
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor =db.rawQuery("select * from "+TABLE_NAME, null);
+        return cursor;
+    }
+    public boolean updateData (String id, String name, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(COL1,id);
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,marks);
+
+        db.update(TABLE_NAME,contentValues,"ID=?",new String[]{id});
+        return true;
+    }
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID=?",new String[]{id});
     }
 }
--- app/src/main/java/com/example/sqliteproject/MainActivity.java
+++ app/src/main/java/com/example/sqliteproject/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.sqliteproject;
 
+import static org.jetbrains.annotations.Nls.Capitalization.Title;
+
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText id,name,marks;
-    Button insert, view, edit,delete;
+    EditText id, name, marks;
+    Button insert, view, edit, delete;
+    DatabaseHelper mdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +28,83 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        id=findViewById(R.id.txt_id);
-        name=findViewById(R.id.txt_name);
-        marks=findViewById(R.id.txt_marks);
-        insert=findViewById(R.id.btn_insert);
-        view=findViewById(R.id.btn_view);
-        edit=findViewById(R.id.btn_Edit);
-        delete=findViewById(R.id.btn_delete);
+        id = findViewById(R.id.txt_id);
+        name = findViewById(R.id.txt_name);
+        marks = findViewById(R.id.txt_marks);
+        insert = findViewById(R.id.btn_insert);
+        view = findViewById(R.id.btn_view);
+        edit = findViewById(R.id.btn_Edit);
+        delete = findViewById(R.id.btn_delete);
 
+        mdb = new DatabaseHelper(this);
+
+//        insert data
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean insertedData = mdb.insertData(
+                        id.getText().toString().trim(),
+                        name.getText().toString().trim(),
+                        marks.getText().toString().trim());
+                if (insertedData == true) {
+                    Toast.makeText(MainActivity.this, "Data is inserted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something Error!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//View All Data
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cur = mdb.getAllData();
+                if (cur.getCount() == 0) {
+                    Toast.makeText(MainActivity.this, "Data is Empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (cur.moveToNext()) {
+                    buffer.append("ID: " + cur.getString(0) + "\n");
+                    buffer.append("Name: " + cur.getString(1) + "\n");
+                    buffer.append("Marks: " + cur.getString(2) + "\n");
+                }
+                show("data", buffer.toString());
+                ")
+            }
+        });
+//        Edit Data
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean updateData = mdb.updateData(
+                        id.getText().toString().trim(),
+                        name.getText().toString().trim(),
+                        marks.getText().toString().trim());
+                if (updateData == true) {
+                    Toast.makeText(MainActivity.this, "Data is Updated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something Error!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//        Delete Data
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer deleted = mdb.deleteData(id.getText().toString().trim());
+                if (deleted > 0) {
+                    Toast.makeText(MainActivity.this, "Data is Deleted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Something Error!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void show(String Title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(Title);
+        builder.setMessage(Message);
     }
 }
